917a8612badf933cd66f428562b4705b3d01ff83
Andrei Sharapov <adras.sad@gmail.com>
2025-04-21 02:38:03 +0300

    fix mistake

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -30,6 +30,14 @@ const config: HardhatUserConfig = {
   },
   solidity: {
     version: "0.8.20",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+      evmVersion: "london",
+      
+    },
   },
 };
 
